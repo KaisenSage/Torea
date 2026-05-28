b459e5de5e5a0e294fdfc29b41b6174947e42eae
Babalola Abdallah <abdallah@192.168.1.10>
2026-05-28 05:57:40 +0100

    Revert "Stop merging cart during checkout"
    This reverts commit 509e168f665f23e9611dceace283ec3f5f026112.

--- server/actions/checkout.ts
+++ server/actions/checkout.ts
@@ -5,6 +5,7 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "@/server/db/prisma";
 import { getCurrentDbUser } from "@/server/auth/rbac";
 import { getCartCookieMap } from "@/server/services/cart-cookie";
+import { mergeCartCookieIntoDatabase } from "@/server/services/cart-merge";
 import { initializePaystackTransaction } from "@/server/services/paystack";
 
 type CheckoutInput = {
@@ -270,6 +271,8 @@ export async function createCheckoutSession(input: CheckoutInput) {
   let items: ResolvedCheckoutItem[];
 
   if (signedInUser) {
+    await mergeCartCookieIntoDatabase(user.id);
+
     try {
       items = await resolveSignedInCartItems(user.id);
     } catch (error) {
